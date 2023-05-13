d2762d94e79a16976fefa47a8f0715c597feb1ad
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-13 14:07:30 +0200

    salary card and list mobile responsiveness

--- src/components/SalaryCard.tsx
+++ src/components/SalaryCard.tsx
@@ -21,15 +21,14 @@ export default SalaryCard
 const Wrapper = styled.article`
 	background-color: ${({ theme }) => theme.appColors.blue};
 	border-radius: ${({ theme }) => theme.borderRadius.extraLarge};
-	box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px,
-		rgba(0, 0, 0, 0.22) 0px 15px 12px;
+	box-shadow: rgba(0, 0, 0, 0.3) 0 19px 38px, rgba(0, 0, 0, 0.22) 0 15px 12px;
 	color: ${({ theme }) => theme.appColors.white};
 	font-family: "Agrandir", sans-serif;
 	margin-inline: auto;
 	padding: 1rem 3.5rem 0.5rem;
 	text-align: center;
 	transition: transform 0.3s ease-in-out;
-	width: fit-content;
+	min-width: min(300px, fit-content);
 
 	h2,
 	h4,
@@ -39,16 +38,31 @@ const Wrapper = styled.article`
 
 	h2 {
 		font-family: "Agrandir Heavy", sans-serif;
-		font-size: 1.7rem;
-	}
-	h4 {
 		font-size: 1.3rem;
 	}
+	h4 {
+		font-family: "Agrandir Bold", sans-serif;
+		font-size: 1.1rem;
+	}
 	p {
-		font-size: 1rem;
+		font-size: 0.8rem;
 	}
 
 	&:hover {
 		transform: scale(1.05);
+	}
+
+	@media (max-width: ${({ theme }) => theme.screenWidth.mobile}) {
+		padding: 0.8rem 1rem 0.5rem;
+
+		h2 {
+			font-size: 1rem;
+		}
+		h4 {
+			font-size: 0.8rem;
+		}
+		p {
+			font-size: 0.5rem;
+		}
 	}
 `
