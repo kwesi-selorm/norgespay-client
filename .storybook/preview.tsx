2c1ec9ae941462d57d8bd586118c65df434d282f
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-14 00:57:36 +0200

    update config to integrate react-router-dom

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,14 +2,19 @@ import { withThemeFromJSXProvider } from "@storybook/addon-styling"
 import { ThemeProvider } from "styled-components"
 import theme from "../src/styles/theme"
 import GlobalStyles from "../src/styles/global-styles.js"
-// import GlobalStyles from "../src/styles/global-styles.js"
+import { MemoryRouter } from "react-router-dom"
 
 export const decorators = [
 	withThemeFromJSXProvider({
 		themes: { theme },
 		Provider: ThemeProvider,
 		GlobalStyles
-	})
+	}),
+	(Story) => (
+		<MemoryRouter initialEntries={["/"]}>
+			<Story />
+		</MemoryRouter>
+	)
 ]
 
 const preview = {
