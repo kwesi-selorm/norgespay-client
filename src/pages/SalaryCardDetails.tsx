eae51f0a955fd1ebc61d98235e53a4ce08bd69fd
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-13 14:08:11 +0200

    rename file

--- src/pages/SalaryCardDetails.tsx
+++
@@ -1,5 +0,0 @@
-const SalaryCardDetails = () => {
-	return <h1>Salary card details</h1>
-}
-
-export default SalaryCardDetails
---
+++ src/pages/SalaryInfo.tsx
@@ -0,0 +1,5 @@
+const SalaryInfo = () => {
+	return <h1>Salary card details</h1>
+}
+
+export default SalaryInfo
