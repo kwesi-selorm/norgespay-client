6a01510a590824d6cc4c27c2bbb9ddf7ffd1b708
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-13 02:18:38 +0200

    modify storybook config

---
+++ src/styles/theme.ts
@@ -0,0 +1,51 @@
+const theme = {
+	colors: {
+		primary: "#007bff",
+		secondary: "#6c757d",
+		success: "#28a745",
+		danger: "#dc3545",
+		warning: "#ffc107",
+		info: "#17a2b8",
+		light: "#f8f9fa",
+		dark: "#343a40"
+	},
+	appColors: {
+		red: "#b83143",
+		blue: "#235ca5",
+		white: "#ffffff"
+	},
+	fonts: {
+		weight: {
+			regular: "400",
+			medium: "500",
+			bold: "700",
+			extrabold: "800",
+			black: "900"
+		},
+		size: {
+			s1: 12,
+			s2: 14,
+			s3: 16,
+			m1: 20,
+			m2: 24,
+			m3: 28,
+			l1: 32,
+			l2: 40,
+			l3: 48
+		}
+	},
+	spacing: {
+		small: "8px",
+		medium: "16px",
+		large: "24px",
+		extraLarge: "32px"
+	},
+	borderRadius: {
+		small: "4px",
+		medium: "8px",
+		large: "16px",
+		extraLarge: "24px"
+	}
+}
+
+export default theme
