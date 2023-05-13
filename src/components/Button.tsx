7799566c36fd9e8297e60be2227cd9125ea9994c
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-13 22:30:21 +0200

    create story for cancel button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,29 @@
 import styled from "styled-components"
 
 type Props = {
+	className?: string
 	innerText: string
-	size?: string
+	size?: "small" | "large" | undefined
 	cancelButton?: boolean
+	onClick?: () => void
+	type: "button" | "submit" | "reset" | undefined
 }
 
-const Button = ({ innerText, size, cancelButton }: Props) => {
+const Button = ({ innerText, size, cancelButton, onClick, type }: Props) => {
 	return (
-		<StyledButton size={size} cancelButton={cancelButton}>
+		<StyledButton
+			size={size}
+			cancelButton={cancelButton}
+			onClick={onClick}
+			type={type}
+		>
 			{innerText}
 		</StyledButton>
 	)
 }
 
 const StyledButton = styled.button<{
-	size: string | undefined
+	size: "small" | "large" | undefined
 	cancelButton: boolean | undefined
 }>`
 	background: ${({ theme, cancelButton }) =>
@@ -29,10 +37,12 @@ const StyledButton = styled.button<{
 		size === "small"
 			? `${theme.spacing.small} ${theme.spacing.medium}`
 			: `${theme.spacing.medium} ${theme.spacing.large}`};
+	transition: font-size 0.2s ease-in-out, background 0.2s ease;
 
 	&:hover {
-		background-color: ${({ theme, cancelButton }) =>
+		background: ${({ theme, cancelButton }) =>
 			cancelButton ? theme.appColors.hoverRed : theme.appColors.hoverBlue};
+		font-size: ${({ size }) => (size === "small" ? "0.95rem" : "1.05rem")};
 	}
 `
 
--- src/stories/components/Button.stories.tsx
+++ src/stories/components/Button.stories.tsx
@@ -11,13 +11,18 @@ export default meta
 type Story = StoryObj<typeof Button>
 
 export const SmallButton: Story = {
-	args: { innerText: "Small button", size: "small" }
+	args: { innerText: "Small button", size: "small", type: "button" }
 }
 
 export const LargeButton: Story = {
-	args: { innerText: "Large button", size: "large" }
+	args: { innerText: "Large button", size: "large", type: "button" }
 }
 
 export const CancelButton: Story = {
-	args: { innerText: "Cancel", size: "small", cancelButton: true }
+	args: {
+		innerText: "Cancel",
+		size: "small",
+		cancelButton: true,
+		type: "button"
+	}
 }
