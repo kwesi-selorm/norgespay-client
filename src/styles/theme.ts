05f2b3d62db556217aa2a8275c397f2fa8bd7a23
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-13 22:33:20 +0200

    update media breakpoints

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -18,10 +18,8 @@ const theme = {
 	},
 	screenWidth: {
 		mobile: "480px",
-		tablet: "768px",
-		laptop: "1024px",
-		desktop: "1280px",
-		extraLarge: "1440px"
+		tablet: "1024px",
+		laptopAndDesktop: "2560px"
 	},
 	fonts: {
 		weight: {
