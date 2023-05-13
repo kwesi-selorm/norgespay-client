25b934df8b0455527dfac61e3288a40638e67ac4
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-13 22:32:45 +0200

    Move routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import "./App.css"
-import SalaryCard from "./components/SalaryCard.tsx"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home.tsx"
+import Salaries from "./pages/Salaries.tsx"
+import SalaryInfo from "./pages/SalaryInfo.tsx"
+import ErrorPage from "./pages/ErrorPage.tsx"
 
 function App() {
 	return (
 		<>
-			<h1>App</h1>
-			<SalaryCard jobTitle={"Seller"} city={"Oslo"} lastModified={"Today"} />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/salaries" element={<Salaries />}>
+						<Route path="/salaries/:id" element={<SalaryInfo />} />
+					</Route>
+					<Route path="*" element={<ErrorPage />} />
+				</Routes>
+			</BrowserRouter>
 		</>
 	)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,39 +5,10 @@ import "./index.css"
 import { ThemeProvider } from "styled-components"
 import theme from "./styles/theme"
 import GlobalStyles from "./styles/global-styles.ts"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Home from "./pages/Home.tsx"
-import Salaries from "./pages/Salaries.tsx"
-import SalaryInfo from "./pages/SalaryInfo.tsx"
-import ErrorPage from "./pages/ErrorPage.tsx"
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Home />,
-		children: [
-			{
-				path: "/salaries",
-				element: <Salaries />,
-				children: [
-					{
-						path: "/:id",
-						element: <SalaryInfo />
-					}
-				]
-			}
-		]
-	},
-	{
-		path: "/*",
-		element: <ErrorPage />
-	}
-])
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
-			<RouterProvider router={router} />
 			<App />
 			<GlobalStyles />
 		</ThemeProvider>
