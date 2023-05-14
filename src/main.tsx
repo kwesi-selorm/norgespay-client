aa8754df0e5761522011fb1200a7ed8340c78323
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-15 01:36:14 +0200

    install react-query

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,17 @@ import "./index.css"
 import { ThemeProvider } from "styled-components"
 import theme from "./styles/theme"
 import GlobalStyles from "./styles/global-styles.ts"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<App />
-			<GlobalStyles />
-		</ThemeProvider>
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider theme={theme}>
+				<App />
+				<GlobalStyles />
+			</ThemeProvider>
+		</QueryClientProvider>
 	</React.StrictMode>
 )
