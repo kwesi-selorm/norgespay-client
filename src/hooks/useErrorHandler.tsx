7fdab40befac4fd11cb522b953fd88e0af8a63c8
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-15 01:46:45 +0200

    wip

---
+++ src/hooks/useErrorHandler.tsx
@@ -0,0 +1,34 @@
+import useMessage from "./useMessage.tsx"
+
+const useErrorHandler = () => {
+	const { showMessage, contextHolder } = useMessage()
+	function handleError(error: any) {
+		if (error.response !== undefined) {
+			showMessage({
+				type: "error",
+				content: error.response.data.message
+			})
+		} else if (error.request !== undefined) {
+			showMessage({
+				type: "error",
+				content: error.request
+			})
+		} else {
+			showMessage({
+				type: "error",
+				content: error.message
+			})
+		}
+		showMessage({
+			type: "error",
+			content: error.toString()
+		})
+	}
+
+	return {
+		handleError,
+		contextHolder
+	}
+}
+
+export default useErrorHandler
--- src/pages/Salaries.tsx
+++ src/pages/Salaries.tsx
@@ -1,24 +1,76 @@
-import { MainSalary } from "../@types/types.ts"
+// import { MainSalary } from "../@types/types.ts"
 import styled from "styled-components"
-import { lazy, Suspense } from "react"
+import { lazy, Suspense, useState } from "react"
 import LoadingIcon from "../components/LoadingIcon.tsx"
+import Button from "../components/Button.tsx"
+import { IoMdAdd } from "react-icons/io"
+import { useQuery } from "@tanstack/react-query"
+import { getSalaries } from "../api/salaries-api.ts"
+import useErrorHandler from "../hooks/useErrorHandler.tsx"
+import CustomModal from "../components/Modal.tsx"
+import { MainSalary } from "../@types/types.ts"
+
 const SalaryList = lazy(() => import("../components/SalaryList.tsx"))
 
 const Salaries = () => {
-	const salaries = [] as MainSalary[]
-	const displayFormat = ""
+	const [modalOpen, setModalOpen] = useState(false)
+	const [displayFormat] = useState("grid")
+
+	const { data, error, isLoading, isError } = useQuery<
+		MainSalary[] | undefined
+	>(["salaries, all"], getSalaries, {
+		refetchOnWindowFocus: false,
+		retry: 2
+	})
+	const { handleError, contextHolder } = useErrorHandler()
+
+	const salaries = data !== undefined ? data : []
+
+	if (isLoading) {
+		return <LoadingIcon />
+	}
+	if (isError) {
+		handleError(error)
+		return
+	}
 
 	return (
 		<Wrapper>
+			{contextHolder}
+			<CustomModal
+				title="Add new salary entry"
+				modalOpen={modalOpen}
+				onCancel={() => setModalOpen(false)}
+				onSubmit={() => {
+					console.log("submit")
+				}}
+			>
+				<h1>Children</h1>
+			</CustomModal>
 			<Suspense fallback={<LoadingIcon />}>
 				<SalaryList salaries={salaries} displayFormat={displayFormat} />
 			</Suspense>
+			<Button
+				addButton={true}
+				className="add-button"
+				icon={<IoMdAdd />}
+				innerText="Add new"
+				onClick={() => setModalOpen(true)}
+				size="small"
+				type="button"
+			/>
 		</Wrapper>
 	)
 }
 
 const Wrapper = styled.div`
 	margin-inline: 20px;
+	margin-top: 5%;
+	text-align: left;
+
+	.add-button {
+		margin-top: 2rem;
+	}
 `
 
 export default Salaries
