42460d058fae5c10c0c73d2d16b65d3ec665d111
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-15 19:28:31 +0200

    implement modal context provider

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,19 @@ import { ThemeProvider } from "styled-components"
 import theme from "./styles/theme"
 import GlobalStyles from "./styles/global-styles.ts"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import ModalContextProvider from "./contexts/ModalContext.tsx"
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<ThemeProvider theme={theme}>
-				<App />
-				<GlobalStyles />
-			</ThemeProvider>
-		</QueryClientProvider>
+		<ModalContextProvider>
+			<QueryClientProvider client={queryClient}>
+				<ThemeProvider theme={theme}>
+					<App />
+					<GlobalStyles />
+				</ThemeProvider>
+			</QueryClientProvider>
+		</ModalContextProvider>
 	</React.StrictMode>
 )
