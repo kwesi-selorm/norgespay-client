f04e5a4c1097ecc8d813551e1fbf6371945689fd
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-16 01:18:12 +0200

    make apiURL dynamic based on environment mode

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
 
-// export const baseURL = "https://norges-pay-server.herokuapp.com/api"
-export const baseURL = "http://localhost:3000/api"
+export const baseURL = import.meta.env.DEV
+	? "http://localhost:3000/api"
+	: "https://norges-pay-server.herokuapp.com/api"
 
 const api = axios.create({
 	baseURL,
