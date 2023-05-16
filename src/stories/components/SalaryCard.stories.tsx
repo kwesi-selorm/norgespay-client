7c6f76998a395beac2ccc72b1b90264a555d24ba
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-17 00:38:19 +0200

    update story args

--- src/stories/components/SalaryCard.stories.tsx
+++ src/stories/components/SalaryCard.stories.tsx
@@ -11,18 +11,48 @@ type Story = StoryObj<typeof SalaryCard>
 
 export const Default: Story = {
 	args: {
-		jobTitle: "Software Engineer",
-		city: "Telemark",
-		lastModified: "2023-05-09T22:46:04.963Z",
-		displayFormat: "grid"
+		displayFormat: "grid",
+		salary: {
+			_id: "123",
+			jobTitle: "Software Engineer",
+			city: "Telemark",
+			lastModified: "2023-05-09T22:46:04.963Z",
+			salaries: [
+				{
+					_id: "123",
+					companySpecificJobTitle: "Software Engineer",
+					salaries: [230000, 540000],
+					experience: 3,
+					__v: 0,
+					lastModified: "2023-05-09T22:46:04.963Z"
+				}
+			],
+			__v: 0,
+			sector: "Healthcare"
+		}
 	}
 }
 
 export const ListCard: Story = {
 	args: {
-		jobTitle: "Software Engineer",
-		city: "Telemark",
-		lastModified: "2023-05-09T22:46:04.963Z",
-		displayFormat: "list"
+		displayFormat: "list",
+		salary: {
+			_id: "123",
+			jobTitle: "Software Engineer",
+			city: "Telemark",
+			lastModified: "2023-05-09T22:46:04.963Z",
+			salaries: [
+				{
+					_id: "123",
+					companySpecificJobTitle: "Software Engineer",
+					salaries: [230000, 540000],
+					experience: 3,
+					__v: 0,
+					lastModified: "2023-05-09T22:46:04.963Z"
+				}
+			],
+			__v: 0,
+			sector: "Healthcare"
+		}
 	}
 }
