e97f760b146ce11197425a93e61d687c87b86acd
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-16 23:56:53 +0200

    complete workflow for submitting API requests and update modal UIs

--- src/pages/SalaryInfo.tsx
+++ src/pages/SalaryInfo.tsx
@@ -1,36 +1,72 @@
 import styled from "styled-components"
 import SalaryTable from "../components/data-display/SalaryTable.tsx"
-import { useContext } from "react"
-import { SalaryContext } from "../contexts/SalaryContext.tsx"
+import { useState } from "react"
 import Button from "../components/Button.tsx"
 import CreateSecondarySalaryEntryModal from "../components/modals/CreateSecondarySalaryEntryModal.tsx"
-import { ModalContext } from "../contexts/ModalContext.tsx"
 import AddSecondarySalaryAmountModal from "../components/modals/AddSecondarySalaryAmountModal.tsx"
+import { useQuery } from "@tanstack/react-query"
+import { MainSalary } from "../@types/types.ts"
+import { getSalary } from "../api/salaries-api.ts"
+import useErrorHandler from "../hooks/useErrorHandler.tsx"
+import LoadingIcon from "../components/LoadingIcon.tsx"
+import { useParams } from "react-router-dom"
+import BackButtonBar from "../components/BackButtonBar.tsx"
 
 const SalaryInfo = () => {
-	const { selectedMainSalary, secondarySalaryId } = useContext(SalaryContext)
-	const { modalOpen, setModalOpen } = useContext(ModalContext)
+	const [createModalOpen, setCreateModalOpen] = useState(false)
+	const [addModalOpen, setAddModalOpen] = useState(false)
 
-	return selectedMainSalary !== null ? (
+	const { id } = useParams()
+	const { data, error, isLoading, isError } = useQuery<MainSalary | undefined>(
+		["salaries", "single", id],
+		() => {
+			if (id === null || id === undefined) return
+			return getSalary(id)
+		},
+		{
+			refetchOnWindowFocus: false,
+			retry: 2
+		}
+	)
+	const { handleError, contextHolder } = useErrorHandler()
+
+	const salary = data
+
+	if (isLoading) {
+		return <LoadingIcon />
+	}
+	if (isError) {
+		handleError(error)
+		return null
+	}
+
+	return salary !== undefined ? (
 		<Wrapper>
-			{modalOpen && secondarySalaryId !== null ? (
-				<AddSecondarySalaryAmountModal title="Add New Salary Amount" />
-			) : (
-				<CreateSecondarySalaryEntryModal
-					title={`Add company-specific ${selectedMainSalary.jobTitle} salary entry`}
-				/>
-			)}
+			{contextHolder}
+			<BackButtonBar />
+			<AddSecondarySalaryAmountModal
+				addModalOpen={addModalOpen}
+				setAddModalOpen={setAddModalOpen}
+				title="Add New Salary Amount"
+			/>
+			<CreateSecondarySalaryEntryModal
+				createModalOpen={createModalOpen}
+				setCreateModalOpen={setCreateModalOpen}
+				title={`Add ${salary.jobTitle.toLowerCase()} salary entry`}
+			/>
+
 			<SalaryTable
-				jobTitle={selectedMainSalary?.jobTitle}
-				city={selectedMainSalary?.city}
-				secondarySalaries={selectedMainSalary?.salaries}
+				jobTitle={salary?.jobTitle}
+				city={salary?.city}
+				secondarySalaries={salary?.salaries}
+				setAddModalOpen={setAddModalOpen}
 			/>
 			<Button
 				addButton={true}
 				className="add-button"
 				innerText="Add new entry"
 				onClick={() => {
-					setModalOpen(true)
+					setCreateModalOpen(true)
 				}}
 				size="large"
 				type="button"
@@ -40,9 +76,12 @@ const SalaryInfo = () => {
 }
 
 const Wrapper = styled.div`
+	margin-inline: auto;
+	margin-top: 5%;
 	width: 80%;
 
 	.add-button {
+		margin-inline: auto;
 		margin-top: 1rem;
 	}
 `
