c3cc3e07b486cf1d58f5001345ed9fe5f3e27a08
Jeffery Adorkor <jead@v-p-s.com>
2023-05-16 14:54:16 +0200

    begin salary information page

--- src/pages/SalaryInfo.tsx
+++ src/pages/SalaryInfo.tsx
@@ -1,5 +1,45 @@
+import styled from "styled-components"
+import SalaryTable from "../components/data-display/SalaryTable.tsx"
+import { useContext } from "react"
+import { SalaryContext } from "../contexts/SalaryContext.tsx"
+import Button from "../components/Button.tsx"
+import CreateSecondarySalaryEntryModal from "../components/modals/CreateSecondarySalaryEntryModal.tsx"
+import { ModalContext } from "../contexts/ModalContext.tsx"
+
 const SalaryInfo = () => {
-	return <h1>Salary card details</h1>
+	const { selectedMainSalary } = useContext(SalaryContext)
+	const { setModalOpen } = useContext(ModalContext)
+
+	return selectedMainSalary !== null ? (
+		<Wrapper>
+			<CreateSecondarySalaryEntryModal
+				title={`Add company-specific ${selectedMainSalary.jobTitle} salary`}
+			/>
+			<SalaryTable
+				jobTitle={selectedMainSalary?.jobTitle}
+				city={selectedMainSalary?.city}
+				secondarySalaries={selectedMainSalary?.salaries}
+			/>
+			<Button
+				addButton={true}
+				className="add-button"
+				innerText="Add new company-specific salary"
+				onClick={() => {
+					setModalOpen(true)
+				}}
+				size="large"
+				type="button"
+			/>
+		</Wrapper>
+	) : null
 }
+
+const Wrapper = styled.div`
+	width: 80%;
+
+	.add-button {
+		margin-top: 1rem;
+	}
+`
 
 export default SalaryInfo
