d2be2734a74e6358c06a7a32ded663b14c144ff4
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-16 19:46:57 +0200

    remove main salary data

--- src/contexts/SalaryContext.tsx
+++ src/contexts/SalaryContext.tsx
@@ -1,18 +1,11 @@
 import React, { createContext, useMemo, useState } from "react"
-import { MainSalary } from "../@types/types.ts"
 
 interface ISalaryContext {
-	selectedMainSalary: MainSalary | null
-	setSelectedMainSalary: (salary: MainSalary | null) => void
 	secondarySalaryId: string | null
 	setSecondarySalaryId: (id: string | null) => void
 }
 
 const InitialSalaryContextState = {
-	selectedMainSalary: null,
-	setSelectedMainSalary: (salary: MainSalary | null) => {
-		console.log(salary)
-	},
 	secondarySalaryId: null,
 	setSecondarySalaryId: (id: string | null) => {
 		console.log(id)
@@ -24,20 +17,16 @@ export const SalaryContext = createContext<ISalaryContext>(
 )
 
 const SalaryContextProvider = ({ children }: { children: React.ReactNode }) => {
-	const [selectedMainSalary, setSelectedMainSalary] =
-		useState<MainSalary | null>(null)
 	const [secondarySalaryId, setSecondarySalaryId] = useState<string | null>(
 		null
 	)
 
 	const value = useMemo(
 		() => ({
-			selectedMainSalary,
-			setSelectedMainSalary,
 			secondarySalaryId,
 			setSecondarySalaryId
 		}),
-		[selectedMainSalary, secondarySalaryId]
+		[secondarySalaryId]
 	)
 
 	return (
