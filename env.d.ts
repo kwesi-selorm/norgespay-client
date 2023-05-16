1f121c5b93bd8b67ea446d4e05ffc63f20b7b2a5
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-16 18:56:15 +0200

    update declaration file and tsconfig

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-	readonly VITE_API_URL_DEV: string
-	readonly VITE_API_URL_PROD: string
+	readonly VITE_API_URL: string
 }
 
 interface ImportMeta {
