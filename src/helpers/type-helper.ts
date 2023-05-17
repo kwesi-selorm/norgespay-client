146ba6571fbb3c0282a619eaa4ba1174459a64d9
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-05-17 10:19:34 +0200

    create date util function implementing luxon

--- src/helpers/type-helper.ts
+++ src/helpers/type-helper.ts
@@ -1,5 +1,12 @@
+import { DateTime } from "luxon"
+
 function formatNumberToCurrency(value: number) {
 	return Intl.NumberFormat("en-US").format(value)
 }
 
-export { formatNumberToCurrency }
+function parseToLocaleDate(date: Date) {
+	const dateString = date.toString()
+	return DateTime.fromISO(dateString).toLocaleString(DateTime.DATETIME_SHORT)
+}
+
+export { formatNumberToCurrency, parseToLocaleDate }
