036d2b5b21697ab0ad4131053b4a2c59b656902b
Jeffery Adorkor <jead@v-p-s.com>
2023-05-19 12:08:31 +0200

    update error page format and styling

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,9 +1,16 @@
 import styled from "styled-components"
 import Button from "../components/Button"
 import { useNavigate } from "react-router-dom"
+import parseError from "../helpers/error-handler"
 
-const ErrorPage = () => {
+type Props = {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	error: any
+}
+
+const ErrorPage = ({ error }: Props) => {
 	const navigate = useNavigate()
+	const errorObj = parseError(error)
 
 	function handleNavigateToHome() {
 		navigate("/")
@@ -11,11 +18,39 @@ const ErrorPage = () => {
 
 	return (
 		<Wrapper>
-			<h1>Oops!</h1>
-			<h3>We could not find the page you were looking for.</h3>
-			<h3>Return to the homepage and try again</h3>
+			<h1>OOPS!</h1>
+			{error !== undefined ? (
+				<>
+					<h3 style={{ fontFamily: "Agrandir Heavy" }}>Something went wrong</h3>
+					{errorObj !== undefined ? (
+						<h3>
+							Status:{errorObj?.status}, Message: {errorObj?.content}
+						</h3>
+					) : (
+						<>
+							<h3>Status: {error?.response?.status}</h3>
+							<br />
+							<h3>Message: {error?.response?.data ?? error.message}</h3>
+							<br />
+							<h3>Status text: {error?.response?.statusText}</h3>
+						</>
+					)}
+				</>
+			) : (
+				<h3>
+					Sorry, we could not find the page you were looking for.
+					<br />
+					Please check the URL to make sure you have the right address.
+				</h3>
+			)}
+			<br />
+			<br />
+			<h3 style={{ fontFamily: "Agrandir Heavy" }}>
+				Return to the homepage and try again
+			</h3>
 			<Button
 				innerText="Return to Home"
+				size="small"
 				type="button"
 				cancelButton={true}
 				onClick={handleNavigateToHome}
@@ -29,20 +64,21 @@ const Wrapper = styled.div`
 	color: ${({ theme }) => theme.appColors.white};
 	margin: 0 auto;
 	min-height: 100vh;
-	padding-top: 20%;
+	padding-top: 10%;
+	padding-inline: 5%;
 	text-align: center;
 
 	h1 {
 		font-family: "Agrandir Heavy", sans-serif;
 		font-size: 3rem;
-		//margin: 0;
 	}
 
 	h3 {
 		margin: 0;
 	}
 	button {
-		margin-top: ${({ theme }) => theme.spacing.large};
+		margin-top: 1rem;
+		margin-inline: auto;
 	}
 `
 
