0642adb81f31275034949a47beb000985165911a
Jeffery Adorkor <jead@v-p-s.com>
2023-05-22 13:12:56 +0200

    Layout story wip

---
+++ src/stories/components/Layout.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from "@storybook/react"
+import Layout, { UserStatus } from "../../components/layout/Layout"
+import { LoggedInUser } from "../../@types/types"
+
+const meta: Meta<typeof Layout> = {
+	title: "Layout/Layout",
+	component: Layout
+}
+export default meta
+
+type Story = StoryObj<typeof Layout>
+
+export const Default: Story = {
+	args: {
+		children: (
+			<UserStatus loggedInUser={{ username: "John Doe" } as LoggedInUser} />
+		)
+	}
+}
