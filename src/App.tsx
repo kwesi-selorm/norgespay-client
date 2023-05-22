28fbf1cdd2d259d5c9b4cb7bda5aa8880df60ed3
Jeffery Adorkor <jead@v-p-s.com>
2023-05-22 13:10:48 +0200

    add login route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home"
 import Salaries from "./pages/Salaries"
 import SalaryInfo from "./pages/SalaryInfo"
 import ErrorPage from "./pages/ErrorPage"
+import Login from "./pages/Login"
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
 					<Route path="/salaries" element={<Salaries />} />
 					<Route path="/salaries/:id" element={<SalaryInfo />} />
 					<Route path="*" element={<ErrorPage />} />
