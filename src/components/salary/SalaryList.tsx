c1467daf9d9a3b089815a35a0de243c830025163
Jeffery Adorkor <jead@v-p-s.com>
2023-05-24 22:47:55 +0200

    prettier format

--- src/components/salary/SalaryList.tsx
+++ src/components/salary/SalaryList.tsx
@@ -11,24 +11,17 @@ const SalaryList = ({ salaries, displayFormat }: Props) => {
 	return (
 		<Wrapper displayFormat={displayFormat}>
 			{salaries.map((salary) => (
-				<SalaryCard
-					key={salary._id}
-					displayFormat={displayFormat}
-					salary={salary}
-				/>
+				<SalaryCard key={salary._id} displayFormat={displayFormat} salary={salary} />
 			))}
 		</Wrapper>
 	)
 }
 
 const Wrapper = styled.div<{ displayFormat: string }>`
-	display: ${({ displayFormat }) =>
-		displayFormat === "grid" ? "grid" : "block"};
+	display: ${({ displayFormat }) => (displayFormat === "grid" ? "grid" : "block")};
 	gap: 1.5rem;
 	grid-template-columns: ${({ displayFormat }) =>
-		displayFormat === "grid"
-			? "repeat(auto-fit, minmax(300px, 1fr))"
-			: "repeat(1fr)"};
+		displayFormat === "grid" ? "repeat(auto-fit, minmax(300px, 1fr))" : "repeat(1fr)"};
 	margin-inline: auto;
 	padding-inline: 1.5rem;
 	width: 90%;
