41bcaefea6da6b2954a52206c3d3a35bde85fd84
Jeffery Adorkor <jead@v-p-s.com>
2023-05-24 23:18:13 +0200

    add modals for editing secondary salaries and amounts, style updates

--- src/components/data-display/SalaryTable.tsx
+++ src/components/data-display/SalaryTable.tsx
@@ -1,37 +1,70 @@
 import styled from "styled-components"
-import { MainSalary } from "../../@types/types"
+import { MainSalary, SecondarySalary } from "../../@types/types"
 import theme from "../../styles/theme"
-import {
-	formatNumberToCurrency,
-	parseToLocaleDate
-} from "../../helpers/type-helper"
+import { formatNumberToCurrency, parseToLocaleDate } from "../../helpers/type-helper"
 import { IoMdAdd } from "react-icons/io"
 import Button from "../Button"
 import EmptyTable from "./EmptyTable"
-import { Dispatch, SetStateAction, useContext } from "react"
-import { SalaryContext } from "../../contexts/SalaryContext"
+import { Dispatch, SetStateAction, useState } from "react"
 import addTableARIA from "../../util/table-aria"
+import DeleteSalaryAmountModal from "../modals/DeleteSalaryAmountModal"
+import { DeleteIcon, EditIcon } from "../../assets/icons"
+import UpdateSecondarySalaryAmountModal from "../modals/UpdateSecondarySalaryAmountModal"
+import AddSecondarySalaryAmountModal from "../modals/AddSecondarySalaryAmountModal"
+import DeleteSecondarySalaryModal from "../modals/DeleteSecondarySalaryModal"
 
 type SalaryTableProps = {
 	jobTitle: string
 	city: string
 	secondarySalaries: MainSalary["salaries"]
-	setAddModalOpen: Dispatch<SetStateAction<boolean>>
 }
 type ContributedSalariesProps = {
 	contributedSalaries: number[]
+	setDeleteModalOpen: Dispatch<SetStateAction<boolean>>
+	setSelectedSecondaryId: Dispatch<SetStateAction<string | null>>
+	secondarySalary: SecondarySalary
+	setSalaryAmount: Dispatch<SetStateAction<number>>
+	setUpdateModalOpen: Dispatch<SetStateAction<boolean>>
+	setDeleteEntryModalOpen: Dispatch<SetStateAction<boolean>>
 }
 
 const ContributedSalaries = ({
-	contributedSalaries
+	contributedSalaries,
+	setDeleteModalOpen,
+	setSelectedSecondaryId,
+	secondarySalary,
+	setSalaryAmount,
+	setUpdateModalOpen,
+	setDeleteEntryModalOpen
 }: ContributedSalariesProps) => {
+	function handleDeleteButtonClick(salary: number) {
+		if (contributedSalaries.length === 1) {
+			setDeleteEntryModalOpen(true)
+			setSelectedSecondaryId(secondarySalary._id)
+		} else {
+			setDeleteModalOpen(true)
+			setSelectedSecondaryId(secondarySalary._id)
+			setSalaryAmount(salary)
+		}
+	}
+
+	function handleEditButtonClick(salary: number) {
+		setUpdateModalOpen(true)
+		setSelectedSecondaryId(secondarySalary._id)
+		setSalaryAmount(salary)
+	}
+
 	return (
 		<Wrapper>
 			{contributedSalaries.map((salary) => {
 				return (
-					<li key={salary} style={{ listStyle: "none" }}>
-						{formatNumberToCurrency(salary)}
-					</li>
+					<div key={salary} className="salary-amount-item">
+						<li style={{ listStyle: "none" }}>{formatNumberToCurrency(salary)}</li>
+						<div className="icons-row">
+							<EditIcon className="edit-button" onClick={() => handleEditButtonClick(salary)} />
+							<DeleteIcon className="delete-button" onClick={() => handleDeleteButtonClick(salary)} />
+						</div>
+					</div>
 				)
 			})}
 		</Wrapper>
@@ -43,67 +76,127 @@ const Wrapper = styled.ul`
 	max-height: 5rem;
 	padding-left: 0;
 
-	li:last-of-type {
+	.salary-amount-item {
+		align-items: center;
+		display: flex;
+		gap: 0.8rem;
 		padding-bottom: ${theme.spacing.small};
+	}
+
+	.edit-button,
+	.delete-button {
+		display: none;
+	}
+
+	.edit-button:hover,
+	.delete-button:hover {
+		cursor: pointer;
+	}
+
+	&:hover {
+		.edit-button,
+		.delete-button {
+			display: inline-block;
+		}
 	}
 `
 
-const SalaryTable = ({
-	jobTitle,
-	city,
-	secondarySalaries,
-	setAddModalOpen
-}: SalaryTableProps) => {
-	const { setSecondarySalaryId } = useContext(SalaryContext)
+const SalaryTable = ({ jobTitle, city, secondarySalaries }: SalaryTableProps) => {
+	const [addModalOpen, setAddModalOpen] = useState(false)
+	const [deleteModalOpen, setDeleteModalOpen] = useState(false)
+	const [deleteEntryModalOpen, setDeleteEntryModalOpen] = useState(false)
+	const [selectedSecondaryId, setSelectedSecondaryId] = useState<string | null>(null)
+	const [salaryAmount, setSalaryAmount] = useState(0)
+	const [updateModalOpen, setUpdateModalOpen] = useState(false)
 
 	addTableARIA()
 
 	return (
-		<StyledEmptyTable>
-			<StyledCaption>
-				<h3 className="content">{`${jobTitle}, ${city}`}</h3>
-			</StyledCaption>
-			<StyledThead>
-				<StyledTr>
-					<StyledTh scope="col">Position</StyledTh>
-					<StyledTh scope="col">Experience (yrs)</StyledTh>
-					<StyledTh scope="col">Salaries (NOK)</StyledTh>
-					<StyledTh scope="col">Last Updated</StyledTh>
-				</StyledTr>
-			</StyledThead>
-			<StyledTbody>
-				{secondarySalaries.map((salary) => (
-					<StyledTr className="body-row" key={salary._id}>
-						<StyledTh data-cell="Position" className="position-cell">
-							{salary.companySpecificJobTitle}
-						</StyledTh>
-						<StyledTd data-cell="Experience (yrs)" className="experience-cell">
-							{salary.experience}
-						</StyledTd>
-						<StyledTd data-cell="Salaries (NOK)" className="salaries-cell">
-							<ContributedSalaries contributedSalaries={salary.salaries} />
-						</StyledTd>
-						<StyledTd data-cell="Last Updated" className="last-updated-cell">
-							{parseToLocaleDate(salary.lastModified)}
-							<Button
-								addButton={true}
-								className="add-button"
-								icon={<IoMdAdd />}
-								innerText=""
-								onClick={() => {
-									setSecondarySalaryId(salary._id)
-									setAddModalOpen(true)
-								}}
-								size="small"
-								type="button"
-							></Button>
-						</StyledTd>
+		<Container>
+			<AddSecondarySalaryAmountModal
+				addModalOpen={addModalOpen}
+				setAddModalOpen={setAddModalOpen}
+				title="Add salary amount"
+				selectedSecondaryId={selectedSecondaryId}
+				setSelectedSecondaryId={setSelectedSecondaryId}
+			/>
+			<DeleteSalaryAmountModal
+				modalOpen={deleteModalOpen}
+				salaryAmount={salaryAmount}
+				setSalaryAmount={setSalaryAmount}
+				setModalOpen={setDeleteModalOpen}
+				selectedSecondaryId={selectedSecondaryId}
+				setSelectedSecondaryId={setSelectedSecondaryId}
+			/>
+			<UpdateSecondarySalaryAmountModal
+				modalOpen={updateModalOpen}
+				previousAmount={salaryAmount}
+				selectedSecondaryId={selectedSecondaryId}
+				setPreviousAmount={setSalaryAmount}
+				setModalOpen={setUpdateModalOpen}
+			/>
+			<DeleteSecondarySalaryModal
+				modalOpen={deleteEntryModalOpen}
+				setModalOpen={setDeleteEntryModalOpen}
+				selectedSecondaryId={selectedSecondaryId}
+				setSelectedSecondaryId={setSelectedSecondaryId}
+			/>
+			<StyledEmptyTable>
+				<StyledCaption>
+					<h3 className="content">{`${jobTitle}, ${city}`}</h3>
+				</StyledCaption>
+				<StyledThead>
+					<StyledTr>
+						<StyledTh scope="col">Position</StyledTh>
+						<StyledTh scope="col">Experience (yrs)</StyledTh>
+						<StyledTh scope="col">Salaries (NOK)</StyledTh>
+						<StyledTh scope="col">Last Updated</StyledTh>
 					</StyledTr>
-				))}
-			</StyledTbody>
-		</StyledEmptyTable>
+				</StyledThead>
+				<StyledTbody>
+					{secondarySalaries.map((salary) => (
+						<StyledTr className="body-row" key={salary._id}>
+							<StyledTh data-cell="Position" className="position-cell">
+								{salary.companySpecificJobTitle}
+							</StyledTh>
+							<StyledTd data-cell="Experience (yrs)" className="experience-cell">
+								{salary.experience}
+							</StyledTd>
+							<StyledTd data-cell="Salaries (NOK)" className="salaries-cell">
+								<ContributedSalaries
+									contributedSalaries={salary.salaries}
+									setDeleteModalOpen={setDeleteModalOpen}
+									setSelectedSecondaryId={setSelectedSecondaryId}
+									secondarySalary={salary}
+									setSalaryAmount={setSalaryAmount}
+									setUpdateModalOpen={setUpdateModalOpen}
+									setDeleteEntryModalOpen={setDeleteEntryModalOpen}
+								/>
+							</StyledTd>
+							<StyledTd data-cell="Last Updated" className="last-updated-cell">
+								{parseToLocaleDate(salary.lastModified)}
+								<Button
+									addButton={true}
+									className="add-button"
+									icon={<IoMdAdd />}
+									innerText=""
+									onClick={() => {
+										setSelectedSecondaryId(salary._id)
+										setAddModalOpen(true)
+									}}
+									size="small"
+									type="button"
+								></Button>
+							</StyledTd>
+						</StyledTr>
+					))}
+				</StyledTbody>
+			</StyledEmptyTable>
+		</Container>
 	)
 }
+
+const Container = styled.div``
 
 const StyledEmptyTable = styled(EmptyTable)`
 	@media screen and (max-width: ${({ theme }) => theme.screenWidth.mobile}) {
@@ -194,9 +287,10 @@ const StyledTd = styled.td`
 	}
 
 	&.last-updated-cell {
-		align-items: flex-start;
+		align-items: center;
 		display: flex;
-		flex-direction: column;
+		gap: 0.5rem;
+		//flex-direction: column;
 
 		.add-button {
 			align-items: center;
@@ -214,6 +308,12 @@ const StyledTd = styled.td`
 			svg {
 				margin: 0;
 			}
+		}
+	}
+
+	@media screen and (max-width: ${({ theme }) => theme.screenWidth.laptopAndDesktop}) {
+		&.last-updated-cell {
+			display: block;
 		}
 	}
 
