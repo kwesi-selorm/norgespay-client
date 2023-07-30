06d5de66478c3c83cff5e548a6e1a8b8a99a6f77
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-07-30 18:51:31 +0200

    add expand and collapse icons

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -3,7 +3,9 @@ import {
 	MdEdit as EditIcon,
 	BsArrowRightSquareFill as MoreArrow,
 	FaUserCircle as UserIcon,
-	IoMdCloseCircle as CloseIcon
+	IoMdCloseCircle as CloseIcon,
+	BiExpandVertical as ExpandIcon,
+	BiCollapseVertical as CollapseIcon
 } from "react-icons/all"
 
-export { DeleteIcon, EditIcon, MoreArrow, UserIcon, CloseIcon }
+export { DeleteIcon, EditIcon, MoreArrow, UserIcon, CloseIcon, ExpandIcon, CollapseIcon }
